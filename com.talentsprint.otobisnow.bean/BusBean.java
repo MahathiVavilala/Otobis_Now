b543896061b9ce2b42f4bcd3b62d30c0f91a8327
Mahathi Vavilala <mahathivavilala97@gmail.com>
2017-06-09 21:29:03 +0530

    Updated Bean Classes

--- com.talentsprint.otobisnow.bean/BusBean.java
+++ com.talentsprint.otobisnow.bean/BusBean.java
@@ -1,88 +1,92 @@
 package com.talentsprint.otobisnow.bean;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.talentsprint.otobisnow.util.MysqlConnection;
+
 public class BusBean {
-    private String busNo;
-    private Integer Capacity;
-    private String isAvailable;
-    private Integer busTypeId;
-    private Integer busCategoryId;
-    private Integer busDepoId;
-    private String busDepo;
-    private String busType;
-    private String busCategory;
+	
+	    private String Registrtion_no;
+	    private String BusType_ID;
+	    private int isAC;
+	    private String Seating_Type;
+	    private String Seatind_Capacity;
+	    
+	    
+		public BusBean(){
+		}
 
-    public BusBean() {
-    }
 
-    public Integer getBusTypeId() {
-        return this.busTypeId;
-    }
+		public String getRegistrtion_no() {
+			return Registrtion_no;
+		}
 
-    public String getBusDepo() {
-        return this.busDepo;
-    }
 
-    public void setBusDepo(String busDepo) {
-        this.busDepo = busDepo;
-    }
+		public void setRegistrtion_no(String registrtion_no) {
+			Registrtion_no = registrtion_no;
+		}
 
-    public String getBusType() {
-        return this.busType;
-    }
 
-    public void setBusType(String busType) {
-        this.busType = busType;
-    }
+		public String getBusType_ID() {
+			return BusType_ID;
+		}
 
-    public String getBusCategory() {
-        return this.busCategory;
-    }
 
-    public void setBusCategory(String busCategory) {
-        this.busCategory = busCategory;
-    }
+		public void setBusType_ID(String busType_ID) {
+			BusType_ID = busType_ID;
+		}
 
-    public void setBusTypeId(Integer busTypeId) {
-        this.busTypeId = busTypeId;
-    }
 
-    public Integer getBusCategoryId() {
-        return this.busCategoryId;
-    }
+		public int getIsAC() {
+			return isAC;
+		}
 
-    public void setBusCategoryId(Integer busCategoryId) {
-        this.busCategoryId = busCategoryId;
-    }
 
-    public Integer getBusDepoId() {
-        return this.busDepoId;
-    }
+		public void setIsAC(int isAC) {
+			this.isAC = isAC;
+		}
 
-    public void setBusDepoId(Integer busDepoId) {
-        this.busDepoId = busDepoId;
-    }
 
-    public String getBusNo() {
-        return this.busNo;
-    }
+		public String getSeating_Type() {
+			return Seating_Type;
+		}
 
-    public void setBusNo(String busNo) {
-        this.busNo = busNo;
-    }
 
-    public Integer getCapacity() {
-        return this.Capacity;
-    }
+		public void setSeating_Type(String seating_Type) {
+			Seating_Type = seating_Type;
+		}
 
-    public void setCapacity(Integer capacity) {
-        this.Capacity = capacity;
-    }
 
-    public String getIsAvailable() {
-        return this.isAvailable;
-    }
+		public String getSeatind_Capacity() {
+			return Seatind_Capacity;
+		}
 
-    public void setIsAvailable(String isAvailable) {
-        this.isAvailable = isAvailable;
-    }
+
+		public void setSeatind_Capacity(String seatind_Capacity) {
+			Seatind_Capacity = seatind_Capacity;
+		}
+    public static boolean isBus(String src, String dest) {
+		System.out.println(src);
+		System.out.println(dest);
+		Connection con = MysqlConnection.getConnection();
+		Statement ps = null;
+		ResultSet rs = null;
+		try {
+
+			ps = con.createStatement();
+			rs = ps.executeQuery("select * from Bus B, BusType BT where B.BusType_ID = BT.BusType_ID and  Registration_No in (select Registration_No from Schedule S, Route R where S.Route_ID = R.Route_ID and R.Source = '"+src+"' and R.Destination = '"+dest+"');");
+			//int i = 0;
+			while (rs.next()) {
+				System.out.println(rs.getString(1));
+				//i++;
+				return true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
