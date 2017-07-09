5e547704f6bac9e6077f33e5cde2390a8fed6805
Mahathi Vavilala <mahathivavilala97@gmail.com>
2017-07-09 15:31:55 +0530

    Added and Modified Controllers

--- com.talentsprint.otobisnow.controller/purchase.java
+++ com.talentsprint.otobisnow.controller/purchase.java
@@ -9,22 +9,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-
-import com.talentsprint.otobisnow.dao.AdminDAO;
 
 /**
- * Servlet implementation class AdminLoginController
+ * Servlet implementation class purchase
  */
-@WebServlet("/AdminController")
-public class AdminController extends HttpServlet {
+@WebServlet("/purchase1")
+public class purchase extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AdminController() {
+    public purchase() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,6 +30,7 @@ public class AdminController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		doPost(request,response);
 	}
 
 	/**
@@ -41,21 +38,10 @@ public class AdminController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		String uname = request.getParameter("user");
-		String password = request.getParameter("pass");
-	    
-		AdminDAO admin = new AdminDAO();
-		 if(admin.isAdmin(uname,password)) {
-			  //out.print("true");
-			RequestDispatcher rd = request.getRequestDispatcher("busManagement.jsp");
-			rd.forward(request, response);  
-			
-		} else {
-			out.print("Invalid Credentials");
-			RequestDispatcher rd = request.getRequestDispatcher("AdminFront.html");
-			rd.forward(request, response);
-		} 
-}
+		RequestDispatcher rs = request.getRequestDispatcher("Message.html");
+		rs.forward(request, response);
+	}
 
 }
